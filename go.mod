82c6afe7c8202bd8c201056b126a43d3fc1d4777
yugj <yugj@hotmail.com>
2020-03-16 22:17:11 +0800

    zipkin log

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module grpctest
 
-require google.golang.org/grpc v1.28.0
+require (
+	github.com/openzipkin/zipkin-go v0.2.2
+	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	google.golang.org/grpc v1.28.0
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
+)
 
 go 1.13
--- src/cn.yugj/server.go
+++ src/cn.yugj/server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/openzipkin/zipkin-go"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"golang.org/x/net/context"
+	zipkingrpc "github.com/openzipkin/zipkin-go/middleware/grpc"
+	logreporter "github.com/openzipkin/zipkin-go/reporter/log"
 )
 
 const (
@@ -19,6 +23,10 @@ func (s *server) SayHello(ctx context.Context, in *HelloRequest) (*HelloReply, e
 	return &HelloReply{Message: "Hello " + in.Name}, nil
 }
 
+func callJava()  {
+
+}
+
 func main() {
 
 	lis, err := net.Listen("tcp", PORT)
@@ -27,10 +35,15 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	//create server with zipkin wrapper
+	reporter := logreporter.NewReporter(log.New(os.Stderr, "", log.LstdFlags))
+	tracer, err := zipkin.NewTracer(reporter)
+	server2 := grpc.NewServer(grpc.StatsHandler(zipkingrpc.NewServerHandler(tracer)))
 
-	RegisterGreeterServer(s, &server{})
+	//s := grpc.NewServer()
+
+	RegisterGreeterServer(server2, &server{})
 
 	log.Println("rpc服务已经开启")
-	s.Serve(lis)
+	server2.Serve(lis)
 }
